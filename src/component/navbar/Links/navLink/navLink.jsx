57ee436c63446a028d7c81ce3381738d0db92dcf
Samarth Gite <76243547+Samarth777415@users.noreply.github.com>
2024-06-28 09:37:01 +0530

    mobile navar close on click

--- src/component/navbar/Links/navLink/navLink.jsx
+++ src/component/navbar/Links/navLink/navLink.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import styles from "./navLink.module.css";
 import { usePathname } from "next/navigation";
 
+
 const NavLink = ({ item }) => {
   const pathName = usePathname();
+  
 
   return (
-    <Link
+    <Link onClick={() => setOpen(false)}
       href={item.path}
       className={`${styles.container} ${
         pathName === item.path && styles.active
